f86442a0b06f0419db847840e8b344a8ac3899a7
Vincent Gustafsson <vincent.mick.gustafsson@gmail.com>
2023-05-18 23:48:06 +0200

    clean up the `Tests` project's `Program` file

--- Tests/Program.fs
+++ Tests/Program.fs
@@ -1,18 +1,5 @@
-module Program =
+module Program
 
-    [<EntryPoint>]
-    let main _ = 0
-
-(**
-module Tests
-
-open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
-
-[<Test>]
-let Test1 () =
-    Assert.Pass()
-**)
+[<EntryPoint>]
+let main _ =
+    1
